0c83e1bce786ab565967a98380dd12858fed28ab
ErNishantNeupane <npn.nishant94@gmail.com>
2026-02-07 23:23:07 -0500

    blogs and search features

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,5 +1,5 @@
 
-
+from assignments.models import SocialLink
 
 from .models import Category
 
@@ -7,3 +7,8 @@ from .models import Category
 def get_categories(request):
     categories = Category.objects.all()
     return dict(categories=categories)
+
+
+def get_social_links(request):
+    social_link = SocialLink.objects.all()
+    return dict(social_links=social_link)
