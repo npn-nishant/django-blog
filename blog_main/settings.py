ab69a1f6efb6c71084d93e5b939bd6ee9ec93dc8
ErNishantNeupane <npn.nishant94@gmail.com>
2026-02-08 19:55:37 -0500

    registration and login functionality

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blogs",
     "assignments",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
+
+
+CRISPY_TEMPLATE_PACK = 'Bootstrap4'
