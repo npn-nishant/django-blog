16a8f006c6ff5919c2b749455461b20b0cc6f478
ErNishantNeupane <npn.nishant94@gmail.com>
2026-02-10 21:46:28 -0500

    Comment Feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
-from .models import Blog, Category
+from .models import Blog, Category, Comment
 from django.db.models import Q
 
 # Create your views here
@@ -21,10 +22,24 @@ def posts_by_category(request,category_id):
 
 def blogs(request, slug):
     single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    if request.method == 'POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+
+    # Comments
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count()
+    
     context = {
-        'single_blog' : single_blog,
+        'single_blog': single_blog,
+        'comments': comments,
+        'comment_count': comment_count,
     }
-    return render(request, 'blogs.html',context)
+    return render(request, 'blogs.html', context)
 
 def search(request):
     keyword = request.GET.get('keyword')
